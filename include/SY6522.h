878e203dc2c55bea1facfdb27030a2ad868e772c
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-05-03 10:59:07 +0200

    feat!(arduino): Refactor SY6522 class source and header to be compatible with embedded devices, where complex STL functions like <map> and <vector> might not work

--- include/SY6522.h
+++ include/SY6522.h
@@ -3,7 +3,6 @@
 
 #include <ChipPins.h>
 #include <cstdint>
-#include <map>
 
 namespace Jynsaillar::SY6522
 {
@@ -11,15 +10,19 @@ namespace Jynsaillar::SY6522
     {
     public:
         SY6522();
-        const std::map<int, Jynsaillar::ChipPins::ChipPin> &Pins = _pins;
+
+        ChipPins::ChipPin *const Pins = pins;           // Pointer to private IC pin array (fixed size)
+        const unsigned int *const PinsSize = &pinsSize; // Pointer to private constant which defines the number of elements in the pins array
 
         void (*DigitalWrite)(uint8_t, uint8_t); // Function to write to pin
-        void (*Write)(int, int); // Function to write to pin
-        int (*Read)(uint8_t);       // Function to read pin state
+        void (*Write)(int, int);                // Function to write to pin
+        int (*Read)(uint8_t);                   // Function to read pin state
 
     private:
-        std::map<int, Jynsaillar::ChipPins::ChipPin> _pins;
+        inline static const unsigned int _pinsSize = 40;
+        ChipPins::ChipPin _pins[_pinsSize];
     };
+
 } // namespace Jynsaillar::SY6522
 
 #endif // __SY6522_H__

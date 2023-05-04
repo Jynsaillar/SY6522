76969732063e899e34bd5815cba6335e9f2b1067
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-05-04 08:08:56 +0200

    feat(DataBus): Add forward declarations for two versions of 'SetDataBus(...)', which sets each bit on the data bus (D7 to D0)

--- include/SY6522.h
+++ include/SY6522.h
@@ -21,11 +21,13 @@ namespace Jynsaillar::SY6522
         int (*Read)(uint8_t);                   // Function to read pin state
         void (*Delay)(unsigned long);           // Function to sleep for <n> ms
 
-        void PulseClock(); // Clock goes high, sleep, low, sleep
-        void EnableChip(); // Pulls CS1 high, CS2 low -> chip active
-        void ReadMode();   // R/W high -> chip is in read mode
-        void WriteMode();  // R/W low -> chip is in write mode
-        void RegisterSelect(uint8_t rs3, uint8_t rs2, uint8_t rs1, uint8_t rs0); // rs3 to rs0 signal which register on the chip is to be written to/read from
+        void PulseClock();                                                                       // Clock goes high, sleep, low, sleep
+        void EnableChip();                                                                       // Pulls CS1 high, CS2 low -> chip active
+        void ReadMode();                                                                         // R/W high -> chip is in read mode
+        void WriteMode();                                                                        // R/W low -> chip is in write mode
+        void RegisterSelect(uint8_t rs3, uint8_t rs2, uint8_t rs1, uint8_t rs0);                 // rs3 to rs0 signal which register on the chip is to be written to/read from
+        void SetDataBus(uint8_t);                                                                // Sets D7 to D0 with bit flags, each bit represents D<X> -> e.g. SetDataBus(0x00001111) sets D7 to D4 = 0, D3 to D0 = 1
+        void SetDataBus(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t); // Sets D7 to D0 explicitly
         // End of Functions
 
     private:
